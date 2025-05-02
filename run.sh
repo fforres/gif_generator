518509daefab7aee7c5df3d07ac0e90b9d09a9bf
fforres <felipe.torressepulveda@gmail.com>
2025-05-02 12:24:53 -0700

    Add yeetgif for osx and update script

--- run.sh
+++ run.sh
@@ -9,11 +9,11 @@ else
 
     echo $NAME && \
     (/bin/rm -f $NAME/$NAME-roll.gif || true) && \
-    <gifs/$NAME/$NAME.png gif crop | gif roll | gif optimize --kb 127 >gifs/$NAME/$NAME-roll.gif && \
+    <gifs/$NAME/$NAME.png ./gif crop | ./gif roll | ./gif optimize --kb 127 >gifs/$NAME/$NAME-roll.gif && \
     (/bin/rm -f $NAME/$NAME-wobble.gif || true) && \
-    <gifs/$NAME/$NAME.png gif crop | gif wobble | gif optimize --kb 127 >gifs/$NAME/$NAME-wobble.gif && \
+    <gifs/$NAME/$NAME.png ./gif crop | ./gif wobble | ./gif optimize --kb 127 >gifs/$NAME/$NAME-wobble.gif && \
     (/bin/rm -f $NAME/$NAME-intensifies.gif || true) && \
-    <gifs/$NAME/$NAME.png gif shake -a 28 -f 3 | gif optimize --kb 127 >gifs/$NAME/$NAME-intensifies.gif
+    <gifs/$NAME/$NAME.png ./gif shake -a 10 -f 1 | ./gif optimize --kb 127 >gifs/$NAME/$NAME-intensifies.gif
 
 
 fi
